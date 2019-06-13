38548574c913e43e5ab04449b4947f00212522c0
또르 <hansol416@naver.com>
2019-06-13 17:11:23 +0900

    UserId 확인

--- getGit.js
+++ getGit.js
@@ -2,18 +2,29 @@ const url = require('url')
 const client = require('cheerio-httpcli')   
 const user_url = "https://github.com/9992"
 const log = console.log
-test = url.parse(user_url,true)
-log("test결과 : ",test)
+
+// url 에 유저 닉네임이 없을 경우 입력을 요청하는 함수
 function checkUrl(userUrl)
 {
     parseUrl =  url.parse(userUrl,true)
-    if(parseUrl['path'] == null){
-        log("사용자 이름을 입력 해야함 ")
+    if(parseUrl['path'] == '/'){ 
+        log("유저 닉 추가 되는 부분")
+        nickName = "9992"
+        addparseUrl = addUserId(parseUrl['hostname'],nickName)
     } else {
         log(parseUrl['path'])
     }
 }
 
+
+// 요청에 대한 응답이 돌아왔을때 유저 닉네임을 링크로 추가 시키는 함수
+function addUserId(userUrl,userName)
+{
+    addUrl = userUrl + '/' + userName
+    return addUrl
+}
+
+// 실질적으로 크롤링 하는 부분
 function gitCrawling()
 {
     const crawlData = {
@@ -47,7 +58,7 @@ function gitCrawling()
 
 
 
-
+// git 함수 실행
 let data = gitCrawling()
 checkUrl("https://github.com/9992")
 checkUrl("https://github.com/")
